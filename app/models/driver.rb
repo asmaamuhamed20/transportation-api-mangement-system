db5464c5369d37b83476d1eb2a653a4550851333
asmaa mohamed <asmaamuhamed71@gmail.com>
2024-01-20 17:40:23 +0300

    Slving conflicts

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,10 +1,4 @@
 class Driver < ApplicationRecord
     has_one :vehicle
-<<<<<<< HEAD
-    has_many :ratings
-=======
     has_many :rides
-    has_many :ride_reviews
-    has_many :ratings, through: :ride_reviews
->>>>>>> system-statistics
 end
