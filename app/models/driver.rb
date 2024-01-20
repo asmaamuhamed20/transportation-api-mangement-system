9934a76f5635f9fa23cebdfb20d2fff418a14bbc
asmaa mohamed <asmaamuhamed71@gmail.com>
2024-01-20 16:52:59 +0300

    Discrded changes on driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,9 +1,4 @@
 class Driver < ApplicationRecord
     has_one :vehicle
-<<<<<<< HEAD
     has_many :ratings
-=======
-    has_many :rides
-    has_many :ratings, through: :rides, source: :ratings
->>>>>>> system-statistics
 end
