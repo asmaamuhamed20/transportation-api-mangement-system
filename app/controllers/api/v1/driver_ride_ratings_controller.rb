bc1bb5713508b81f58af8a25013f85fab3a13fb3
asmaa mohamed <asmaamuhamed71@gmail.com>
2024-01-22 13:53:27 +0300

    Resolved driver rating methods

--- app/controllers/api/v1/driver_ride_ratings_controller.rb
+++ app/controllers/api/v1/driver_ride_ratings_controller.rb
@@ -1,58 +1,45 @@
 class Api::V1::DriverRideRatingsController < ApplicationController
     before_action :set_ride
-    before_action :authorize_admin, only: [:create] 
-
-    
+    before_action :authorize_admin, only: [:create]
+  
     def create
-        # driver reviews a user
-        @rating = @ride.driver_ride_ratings.build(rating_params.merge(user: @ride.user, driver: @ride.driver))
-        
-        if @rating.save
-        render json: @rating, status: :created
-        else
-            render_json_success( @rating.errors.full_messages)
-        end
+      @rating = @ride.driver_ride_ratings.build(rating_params.merge(user: @ride.user, driver: @ride.driver))
+  
+      if @rating.save
+        render json: { success: true, data: @rating }, status: :created
+      else
+        render_error(:unprocessable_entity, @rating.errors.full_messages)
+      end
     end
-
+  
     def show
-        render json: @rating
+      render json: { success: true, data: @rating }
     end
-
+  
     def index
-        @ratings = DriverRideRating.where(ride_id: params[:ride_id])
-        render json: @ratings
+      @ratings = DriverRideRating.where(ride_id: params[:ride_id])
+      render json: { success: true, data: @ratings }
     end
-
-    def update
-        
-    end
-
-    def destroy
-        
-    end
-
+  
     private
-
+  
     def authorize_admin
-        unless current_user.admin?
-          render json: { error: 'Not authorized' }, status: :unauthorized
-        end
+      unless current_user.admin?
+        render_error(:unauthorized, 'Not authorized')
+      end
     end
-
+  
     def set_ride
-        @ride = Ride.find(params[:ride_id])
+      @ride = Ride.find(params[:ride_id])
     end
-
+  
     def rating_params
-        params.require(:driver_ride_rating).permit(:ride_id, :user_id, :driver_id, :rating_value, :comment)
-    end
-
-    
-    def render_error(status, messages)
-        render json: { error: messages }, status: status
+        params.require(:rating).permit(:rating_value, :comment)
     end
       
-    def render_json_success(message, data = {})
-        render json: { success: true, message: message, data: data }, status: :ok
-    end 
+  
+    def render_error(status, message)
+      render json: { success: false, error: message }, status: status
+    end
 end
+  
