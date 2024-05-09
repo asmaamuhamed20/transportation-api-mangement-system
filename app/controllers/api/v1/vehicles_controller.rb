e272210bc2fd9aa5e766515bedf963fa255191cc
asmaa mohamed <asmaamuhamed71@gmail.com>
2024-05-09 21:34:24 +0300

    Rfactoring Vehicle controller

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -1,65 +1,77 @@
-module Api
-    module V1
-      class VehiclesController < ApplicationController
-        before_action :set_vehicle, only: [:show, :edit, :update, :destroy]
-        before_action :authorize_admin
+class Api::V1::VehiclesController < ApplicationController
+  before_action :set_vehicle, only: [:show, :update, :destroy]
+  before_action :authorize_admin,  only: [:create, :update, :destroy]
+
+  def index
+    render json: Vehicle.all
+  end
+
+  def show
+    render_json_success(@vehicle)
+  end
+
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    handle_save(@vehicle)
+  end
+
+  def update
+    handle_update(@vehicle, vehicle_params)
+  end
+
+  def destroy
+    handle_destroy(@vehicle)
+  end
+
+  private
   
-        def index
-          render json: Vehicle.all
-        end
-  
-        def show
-          render json: @vehicle
-        end
-  
-        def new
-          @vehicle = Vehicle.new
-        end
-  
-        def create
-          @vehicle = Vehicle.new(vehicle_params)
-          if @vehicle.save
-            render json: @vehicle, status: :created
-          else
-            render json: { error: @vehicle.errors.full_messages }, status: :unprocessable_entity
-          end
-        end
-  
-        def edit
-            
-        end
-  
-        def update
-          if @vehicle.update(vehicle_params)
-            render json: @vehicle
-          else
-            render json: { error: @vehicle.errors.full_messages }, status: :unprocessable_entity
-          end
-        end
-  
-        def destroy
-          if @vehicle.destroy
-            render json: { success: true, message: 'Vehicle removed successfully' }, status: :ok
-          else
-            render json: { error: @vehicle.errors.full_messages }, status: :unprocessable_entity
-          end
-        end
-  
-        private
-        
-        def authorize_admin
-          unless current_user.admin?
-            render json: { error: 'Not authorized' }, status: :unauthorized
-          end
-        end
-        def set_vehicle
-          @vehicle = Vehicle.find(params[:id])
-        end
-  
-        def vehicle_params
-          params.require(:vehicle).permit(:model, :registration_number, :driver_id, :available_time)
-        end
-      end
+
+  def set_vehicle
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def handle_save(vehicle)
+    if vehicle.save
+      render_json_success(vehicle, :created)
+    else
+      render_json_error(vehicle.errors.full_messages.join(', '))
     end
+  end
+
+  def handle_update(vehicle, vehicle_params)
+    if vehicle.update(vehicle_params)
+      render_json_success(vehicle)
+    else
+      render_json_error(vehicle.errors.full_messages.join(', '))
+    end
+  end
+
+  def handle_destroy(vehicle)
+    if vehicle.destroy
+      render_json_success(message: 'Vehicle removed successfully')
+    else
+      render_json_error(vehicle.errors.full_messages.join(', '))
+    end
+  end
+
+  def authorize_admin
+    render_json_error('Not authorized', :unauthorized) unless current_user.admin?
+  end
+
+  def vehicle_params
+    params.require(:vehicle).permit(:model, :registration_number, :driver_id, :available_time)
+  end
+
+  def render_json_success(data, status = :ok)
+    render json: { success: true, data: data }, status: status
+  end
+
+  def render_json_error(message, status = :unprocessable_entity)
+    render json: { success: false, error: message }, status: status
+  end
 end
   
