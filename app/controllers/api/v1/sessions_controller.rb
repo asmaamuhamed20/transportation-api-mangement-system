6c1813d334f259ee9fc10c34f9a5270c058d71fa
asmaa mohamed <asmaamuhamed71@gmail.com>
2024-05-14 03:33:49 +0300

    Refactor SessionsController for improved readability and maintainability

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,44 +1,40 @@
 class Api::V1::SessionsController < Devise::SessionsController
-  before_action :sign_in_params, only: :create
-  before_action :load_user, only: :create
+  before_action :authenticate_user, only: :create
   respond_to :json
 
-  # sign in
+   # POST /api/v1/sign_in
   def create
-    if @user.valid_password?(sign_in_params[:password])
+    if @user.valid_password?(user_params[:password])
       sign_in  @user
-
-      token = JsonWebToken.encode(user_id: @user.id)
-
-      render json: {
-        messages: "Signed In Successfully",
-        is_success: true,
-        data: {user: @user, token: token}
-      }, status: :ok
+      render_sign_in_success
     else
-      render json: {
-        messages: "Signed In Failed - Unauthorized",
-        is_success: false,
-        data: {}
-      }, status: :unauthorized
+      render_sign_in_failure
     end
   end
 
   private
-  def sign_in_params
+
+  def user_params
     params.require(:user).permit(:email, :password)
   end  
 
-  def load_user
-    @user = User.find_for_database_authentication(email: sign_in_params[:email])
-    if @user
-      return @user
-    else
-      render json: {
-        messages: "Cannot get User, please register new user",
-        is_success: false,
-        data: {}
-      }, status: 500 
-    end
+  def authenticate_user
+    @user = User.find_for_database_authentication(email: user_params[:email])
+    return if @user
+  
+    render_unauthorized("Cannot find user. Please register.")
+  end
+  
+  def render_unauthorized(message)
+    render json: { messages: message, is_success: false, data: {} }, status: :unauthorized
+  end
+
+  def render_sign_in_success
+    token = JsonWebToken.encode(user_id: @user.id)
+    render json: { messages: "Signed In Successfully", is_success: true, data: { user: @user, token: token } }, status: :ok
+  end
+
+  def render_sign_in_failure
+    render json: { messages: "Signed In Failed - Unauthorized", is_success: false, data: {} }, status: :unauthorized
   end
 end
