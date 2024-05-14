49c7adf72f424507b63f78ec44686fa57e4cbace
asmaa mohamed <asmaamuhamed71@gmail.com>
2024-05-14 03:24:56 +0300

    Refactor Registrations controller for cleaner code and Reorganize routes file for better readability

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,34 +1,40 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
   before_action :ensure_params_exist, only: :create
-  # sign up
+  
+  # POST /api/v1/sign_up
   def create
     user = User.new user_params
     if user.save
-      render json: {
-        messages: "Sign Up Successfully",
-        is_success: true,
-        data: {user: user.slice(:id, :email, :created_at, :updated_at, :username)}
-      }, status: :ok
+      render_success(user)
     else
-      render json: {
-        messages: "Sign Up Failded",
-        is_success: false,
-        data: {}
-      }, status: :unprocessable_entity
+      render_error("Sign Up Failed", :unprocessable_entity)
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :username)
   end  
 
   def ensure_params_exist
     return if params[:user].present?
+    render_error("Missing Params", :bad_request)
+  end
+
+  def render_success(user)
     render json: {
-        messages: "Missing Params",
-        is_success: false,
-        data: {}
-      }, status: :bad_request
+      messages: "Sign Up Successfully",
+      is_success: true,
+      data: { user: user.slice(:id, :email, :created_at, :updated_at, :username) }
+    }, status: :ok
+  end
+
+  def render_error(message, status)
+    render json: {
+      messages: message,
+      is_success: false,
+      data: {}
+    }, status: status
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,75 +1,77 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api do
     namespace :v1 do
-      get 'system_statistics/total_rides_count'
-      get 'system_statistics/daily_rides_count'
-      get 'system_statistics/total_drivers_count'
-      get 'system_statistics/highest_rides_users'
-      get 'system_statistics/highest_rides_drivers'
-      get 'system_statistics/highest_rides_users_by_month/:month/:year', to: 'system_statistics#highest_rides_users_by_month'
-      get 'system_statistics/highest_rides_drivers_by_month/:month/:year',  to: 'system_statistics#highest_rides_drivers_by_month'
-      get 'system_statistics/calculate_usage_percentage'
-      get 'system_statistics/rides_for_driver', to: 'system_statistics#rides_for_driver'
-      get 'user_ratings/average_rating_by_user', to: 'user_ratings#average_rating_by_user'
+      # System Statistics
+      resources :system_statistics, only: [] do
+        collection do
+          get 'total_rides_count'
+          get 'daily_rides_count'
+          get 'total_drivers_count'
+          get 'highest_rides_users'
+          get 'highest_rides_drivers'
+          get 'highest_rides_users_by_month/:month/:year', to: 'system_statistics#highest_rides_users_by_month'
+          get 'highest_rides_drivers_by_month/:month/:year',  to: 'system_statistics#highest_rides_drivers_by_month'
+          get 'calculate_usage_percentage'
+          get 'rides_for_driver', to: 'system_statistics#rides_for_driver'
+        end
+      end
 
+      # Devise Authentication
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+      end
+
+      # Users
       resources :users, only: [] do
         collection do
-        get 'me/rides', to: 'users#view_rides'
-        get 'rides_for_date', to: 'users#rides_for_date'
+          get 'me/rides', to: 'users#view_rides'
+          get 'rides_for_date', to: 'users#rides_for_date'
+        end
       end
-    end
 
-    resources :drivers do
-      member do
-        put 'update', to: 'drivers#update'
+      # Drivers
+      resources :drivers, only: [:index, :show, :create, :update, :destroy] do
+        member do
+          put 'update', to: 'drivers#update'
+          get 'rides_for_driver'
+        end
+      end
+
+      # Driver Ride Ratings
+      resources :driver_ride_ratings, only: [:create, :index, :show] do 
+        member do
+          get 'average_rating_for_driver'
+        end
+      end
+
+      # User Ratings
+      resources :user_ratings, only: [] do
+        collection do
+          get 'average_rating_by_user'
+        end
+      end
+      
+      
+      # Vehicles
+      resources :vehicles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+      # Rides
+      resources :rides do
+        member do
+          patch 'swap_vehicle'
+          post 'add_user', to: 'rides#add_user_to_ride'
+          delete 'remove_user', to: 'rides#remove_user'
+          post 'replace_user'
+          get 'rides_for_user'
+          get 'rides_for_date'
+          get 'rides_for_time_range'
+          post 'complete_ride'
+        end
       end
     end
-    resources :driver_ride_ratings, only: [:create, :index], controller: 'driver_ride_ratings'
   end
-end
-
-    devise_for :users
-    namespace :api, defaults: {format: :json} do
-      namespace :v1 do 
-        devise_scope :user do
-          post "sign_up", to: "registrations#create"
-          post "sign_in", to: "sessions#create"
-        end
-
-        resources :vehicles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-
-        resources :rides do
-
-          resources :driver_ride_ratings, only: [:create], controller: 'driver_ride_ratings'
-
-          member do
-            patch 'swap_vehicle'
-            post 'add_user', to: 'rides#add_user_to_ride'
-            delete 'remove_user', to: 'rides#remove_user'
-            post 'replace_user'
-            get 'rides_for_user'
-            get 'rides_for_date'
-            get 'rides_for_time_range'
-            post 'complete_ride'
-          end
-        end
-
-        resources :drivers, only: [:index, :show, :create, :update, :destroy] do
-          member do
-            get 'rides_for_driver'
-          end
-        end
-
-        resources :driver_ride_ratings, only: [:show] do
-          member do
-            get 'average_rating_for_driver'
-          end
-        end
-
-        resources :users, only: [:index, :create, :update, :destroy]
-      end
-    end
-
-
+   # Health Check
   get "up" => "rails/health#show", as: :rails_health_check
 end
