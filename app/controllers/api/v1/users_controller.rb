6642be316c570bb15a3b1e365ba5baf464313eed
asmaa mohamed <asmaamuhamed71@gmail.com>
2024-07-02 19:51:28 +0300

    Move business logic from UsersController to User model, Added methods to User model for user creation, updating, deletion

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
 
   # GET: /api/v1/users
   def index
-    render json: users, status: :ok
+    render json: User.all, status: :ok
   end
 
   def show
@@ -11,28 +11,31 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create  
-    if save_user
-      render json: user, status: :created
+    result = User.create_user(user_params)
+    if result[:status] == :created
+      render json: result[:user], status: :created
     else
-      render_errors(user.errors.full_messages)
+      render_errors(result[:errors])
     end
   end
 
   # PUT: /api/v1/users/user_id
   def update
-    if user.update(user_params)
-      render json: user, status: :ok
+    result = user.update_user(user_params)
+    if result[:status] == :ok
+      render json: result[:user], status: :ok
     else
-      render_errors(user.errors.full_messages)
+      render_errors(result[:errors])
     end
   end
 
   # DELETE: /api/v1/users/user_id
   def destroy
-    if destroy_user_resources(user)
-      render_success("User and associated resources deleted successfully")
+    result = user.destroy_user
+    if result[:status] == :ok
+      render_success(result[:message])
     else
-      render_error(:unprocessable_entity, "Failed to delete user and associated resources")
+      render_error(:unprocessable_entity, result[:message])
     end
   end
   
@@ -43,14 +46,11 @@ class Api::V1::UsersController < ApplicationController
   
   # GET: /api/v1/users/rides_for_date?date=
   def rides_for_date
-    render_rides(rides_for_current_user)
+    rides = User.rides_for_date(current_user, params[:date])
+    render_rides(rides)
   end
 
   private
-
-  def users
-    User.all
-  end
 
   def user
     @user ||= User.find(params[:id])
@@ -59,18 +59,9 @@ class Api::V1::UsersController < ApplicationController
   def authorize_admin
     render_unauthorized unless current_user&.admin?
   end
-
-  def save_user
-    user = User.new(user_params)
-    user.save
-  end 
   
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation, :role)
-  end
-
-  def destroy_user_resources(user)
-    user.rides.destroy_all && user.driver_ride_ratings.destroy_all && user.destroy
   end
 
   def render_rides(rides)
@@ -78,14 +69,6 @@ class Api::V1::UsersController < ApplicationController
       render json: rides, status: :ok
     else
       render_error(:not_found, 'No rides found')
-    end
-  end
-
-  def rides_for_current_user
-    if current_user.admin?
-      Ride.where('DATE(start_time) = ?', params[:date]).includes(:driver, :vehicle).order(start_time: :desc)
-    else
-      current_user.rides.where('DATE(start_time) = ?', params[:date]).includes(:driver, :vehicle).order(start_time: :desc)
     end
   end
 
@@ -103,5 +86,9 @@ class Api::V1::UsersController < ApplicationController
 
   def render_unauthorized
     render json: { error: "Only admin users are authorized to perform this action" }, status: :unauthorized
+  end
+
+  def render_not_found(message)
+    render json: { error: message }, status: :not_found
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,37 @@ class User < ApplicationRecord
   def admin?
     role == 'admin'
   end
+
+  def self.create_user(user_params)
+    user = User.new(user_params)
+    if user.save
+      { status: :created, user: user }
+    else
+      { status: :unprocessable_entity, errors: user.errors.full_messages }
+    end
+  end
+
+  def update_user(user_params)
+    if update(user_params)
+      { status: :ok, user: self }
+    else
+      { status: :unprocessable_entity, errors: errors.full_messages }
+    end
+  end
+
+  def destroy_user
+    if rides.destroy_all && driver_ride_ratings.destroy_all && destroy
+      { status: :ok, message: "User and associated resources deleted successfully" }
+    else
+      { status: :unprocessable_entity, message: "Failed to delete user and associated resources" }
+    end
+  end
+
+  def self.rides_for_date(user, date)
+    if user.admin?
+      Ride.where('DATE(start_time) = ?', date).includes(:driver, :vehicle).order(start_time: :desc)
+    else
+      user.rides.where('DATE(start_time) = ?', date).includes(:driver, :vehicle).order(start_time: :desc)
+    end
+  end
 end
