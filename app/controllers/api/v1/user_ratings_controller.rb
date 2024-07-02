21f4cc9aecfec34e90836574e987cce8c56ece29
asmaa mohamed <asmaamuhamed71@gmail.com>
2024-07-02 21:37:41 +0300

    Move user rating logic, Added validations, Updated UserRatingsController

--- app/controllers/api/v1/user_ratings_controller.rb
+++ app/controllers/api/v1/user_ratings_controller.rb
@@ -3,12 +3,11 @@ class Api::V1::UserRatingsController < ApplicationController
     
     # POST: /api/v1/user_ratings
     def create
-        build_user_rating
-    
-        if @user_rating.save
-            render_json_success("User rating created successfully", @user_rating)
+        result = UserRating.create_user_rating(@ride, user_rating_params, current_user)
+        if result[:success]
+          render_json_success("User rating created successfully", result[:user_rating])
         else
-            render_error(:unprocessable_entity, @user_rating.errors.full_messages)
+          render_error(:unprocessable_entity, result[:error])
         end
     end
 
@@ -20,13 +19,11 @@ class Api::V1::UserRatingsController < ApplicationController
 
     # GET: /api/v1/user_ratings/average_rating_by_user?user_id=
     def average_rating_by_user 
-        user_id = params[:user_id]
-        user_ratings = UserRating.where(user_id: user_id)
-        if user_ratings.present?
-            average_rating = calculate_average_rating(user_ratings)
-            render_json_success({ user_id: user_id, average_rating: average_rating })
-        else     
-            render_error(:not_found, 'No ratings available for the user') 
+        result = UserRating.average_rating_by_user(params[:user_id])
+        if result[:success]
+          render_json_success({ user_id: params[:user_id], average_rating: result[:average_rating] })
+        else
+          render_error(:not_found, result[:error])
         end
     end
 
@@ -36,25 +33,15 @@ class Api::V1::UserRatingsController < ApplicationController
     def set_ride
         ride_id = params.dig(:user_rating, :ride_id)
         @ride = Ride.find_by(id: ride_id)
-      
-        return if @ride && @ride.can_be_rated_by?(current_user)
-      
-        render_error(:unprocessable_entity, @ride ? "Current user is not allowed to rate this ride" : "Invalid ride ID")
+ 
+        unless @ride && @ride.can_be_rated_by?(current_user)
+            render_error(:unprocessable_entity, @ride ? "Current user is not allowed to rate this ride" : "Invalid ride ID")
+        end
     end
           
 
     def user_rating_params
         params.require(:user_rating).permit(:comment, :rating, :ride_id)
-    end
-
-    def build_user_rating
-        @user_rating = @ride.user_ratings.build(user_rating_params.merge(user_id: current_user.id))
-    end
-    
-    def calculate_average_rating(user_ratings)
-        total_ratings = user_ratings.count
-        sum_of_ratings = user_ratings.sum(:rating)
-        average_rating = sum_of_ratings.to_f / total_ratings
     end
 
     def render_json_success(message, data = {})
--- app/models/user_rating.rb
+++ app/models/user_rating.rb
@@ -1,5 +1,31 @@
 class UserRating < ApplicationRecord
   belongs_to :ride
   belongs_to :user
-  has_many :driver_ride_ratings, dependent: :destroy
+
+  validates :comment, presence: true
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+
+  def self.create_user_rating(ride, user_rating_params, user)
+    return { success: false, error: "Current user is not allowed to rate this ride" } unless ride.can_be_rated_by?(user)
+
+    user_rating = ride.user_ratings.build(user_rating_params.merge(user_id: user.id))
+
+    if user_rating.save
+      { success: true, user_rating: user_rating }
+    else
+      { success: false, error: user_rating.errors.full_messages }
+    end
+  end
+
+  def self.average_rating_by_user(user_id)
+    user_ratings = where(user_id: user_id)
+    if user_ratings.present?
+      total_ratings = user_ratings.count
+      sum_of_ratings = user_ratings.sum(:rating)
+      average_rating = sum_of_ratings.to_f / total_ratings
+      { success: true, average_rating: average_rating }
+    else
+      { success: false, error: 'No ratings available for the user' }
+    end
+  end
 end
